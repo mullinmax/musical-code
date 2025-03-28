985911785c28458e55bc20d7be0e5790d8c2ef96
maxwell mullin <mullinmax@gmail.com>
2025-03-27 20:54:38 -0400

    updated billie jean

--- billieJean.py
+++ billieJean.py
@@ -1,4 +1,14 @@
-if BillieJean != my.lover:
-    BillieJean = "justa girl"
-    BillieJean.assert("i = 1")
-}
+class Person:
+    def __init__(self, relationship):
+        self.relationship = relationship
+
+BillieJean = Person("just a girl")
+
+i = 0
+if BillieJean.relationship != "lover":
+    print("Billie Jean is not my lover")
+    print(f"She's: {BillieJean.relationship}")
+    assert i == 1
+else:
+    print("Billie Jean is my lover")
+
